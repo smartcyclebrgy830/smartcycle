28d421495f9fa4f3c2948515e01c3c2437cabe08
vivi <vllr.jana@gmail.com>
2026-06-20 22:32:57 +0800

    Update junkshop_export.js

--- junkshop_export.js
+++ junkshop_export.js
@@ -175,7 +175,7 @@ const JunkshopExport = (() => {
         const { dataGrid, materialsList } = opts.reportData || await aggregateSupabaseData(month, year);
         const monthLabel = `${MONTHS[month]} ${year}`;
 
-        const doc = new jsPDF({ orientation: 'landscape', unit: 'pt', format: 'a4' });
+        const doc = new jsPDF({ orientation: 'landscape', unit: 'pt', format: [612, 936] });
         const W = doc.internal.pageSize.getWidth();
         const H = doc.internal.pageSize.getHeight();
         const ML = 30, MR = 30;
