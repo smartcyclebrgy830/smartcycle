731cb20dd7828df556b8097aea5a9fc756ad61ec
Louis Villaflor <127172930+smartcyclebrgy830@users.noreply.github.com>
2026-06-21 19:52:24 +0800

    Update add_collection.js

--- add_collection.js
+++ add_collection.js
@@ -4,7 +4,7 @@ window.currentCategory = typeof window.currentCategory !== 'undefined' ? window.
 window.currentItems = window.currentItems || []; // Initializing to prevent undefined array
 
 document.addEventListener('DOMContentLoaded', () => {
-// Local cache to resolve names during edit mode 
+// Local cache to resolve names during edit 
 let loadedPricesCache = [];
 
 function generateDisplayId(prefix) {
